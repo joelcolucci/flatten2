846761287ff6f17897257e0a2263d3e1cc8342ec
Joel Colucci <joelcolucci@gmail.com>
2016-10-01 09:07:59 -0400

    feat: namespace functions under f2 namespace

--- src/flatten2.js
+++ src/flatten2.js
@@ -1,155 +1,162 @@
 /**
- * 
- * Adapted/refactored from konklone adaptation of csvkit's recursive JSON flattening algorithm 
+ *
+ * Adapted/refactored from konklone adaptation of csvkit's recursive JSON flattening algorithm
  * Konklone JSON - https://github.com/konklone/json/blob/gh-pages/assets/site.js#L28-L55
  * onyxfish csvkit - https://github.com/onyxfish/csvkit/blob/61b9c208b7665c20e9a8e95ba6eee811d04705f0/csvkit/convert/js.py#L15-L34
- * 
+ *
  */
 
 
-var EMPTY_STRING = "";
-var DELIMITER = "/";
+// (function(undefined) {
+  'use strict';
+
+  var f2 = f2 || {};
 
 
-/**
- * 
- * flattenObect
- * @param {object} obj - to flattening
- * @param {string} path - key path to current key
- * @returns {object}
- * 
- */
+  var EMPTY_STRING = "";
+  var DELIMITER = "/";
 
-function flatten(obj, path) {
+
+  /**
+   *
+   * flattenObect
+   * @param {object} obj - to flattening
+   * @param {string} path - key path to current key
+   * @returns {object}
+   *
+   */
+
+  f2.flatten = function(obj, path) {
     if (path == undefined) {
-        path = EMPTY_STRING;
+      path = EMPTY_STRING;
     }
 
     var result = {};
 
-    if (_isArray(obj) || _isObject(obj)) {
-        for (var key in obj) {
-            var keyPath = _createKeyPath(path, key, DELIMITER);
-            
-            var flattenedObject = flatten(obj[key], keyPath); // Recursive call
-            
-            $.extend(result, flattenedObject);
-        }
-    } else if (_isScalar(obj)) {
-        var endPath = _stripRight(path, DELIMITER);
-        result[endPath] = obj;
+    if (f2._isArray(obj) || f2._isObject(obj)) {
+      for (var key in obj) {
+        var keyPath = f2._createKeyPath(path, key, DELIMITER);
+
+        var flattenedObject = f2.flatten(obj[key], keyPath); // Recursive call
+
+        $.extend(result, flattenedObject);
+      }
+    } else if (f2._isScalar(obj)) {
+      var endPath = f2._stripRight(path, DELIMITER);
+      result[endPath] = obj;
     }
 
     return result;
-}
+  };
 
 
-/**
- * 
- * flattenMany
- * @param {array} data
- * @return {array}
- * 
- */
+  /**
+   *
+   * flattenMany
+   * @param {array} data
+   * @return {array}
+   *
+   */
 
-function flattenMany(data) {
+  f2.flattenMany = function(data) {
     var flattenedObjects = [];
 
     for (var i = 0; i < data.length; i++) {
-        var currentObject = data[i];
+      var currentObject = data[i];
 
-        var flatObject = flatten(currentObject);
+      var flatObject = f2.flatten(currentObject);
 
-        flattenedObjects.push(flatObject);
+      flattenedObjects.push(flatObject);
     }
 
     return flattenedObjects;
-}
+  };
 
 
-/**
- * 
- * _createKeyPath
- * @param {string} path
- * @param {string} key
- * @param {string} delimiter
- * @return {string}
- * 
- */
+  /**
+   *
+   * _createKeyPath
+   * @param {string} path
+   * @param {string} key
+   * @param {string} delimiter
+   * @return {string}
+   *
+   */
 
-function _createKeyPath(path, key, delimiter) {
+  f2._createKeyPath = function(path, key, delimiter) {
     return [
-        path,
-        key,
-        DELIMITER
+      path,
+      key,
+      DELIMITER
     ].join('');
-}
+  };
 
 
-/**
- * 
- * _stripRight
- * @param {string} str
- * @param {string} match
- * @return {string}
- * 
- */
+  /**
+   *
+   * _stripRight
+   * @param {string} str
+   * @param {string} match
+   * @return {string}
+   *
+   */
 
-function _stripRight(str, match) {
+  f2._stripRight = function(str, match) {
     var START_INDEX = 0;
     var END_INDEX = str.length - 1;
 
     if (str[END_INDEX] === match) {
-        return str.substr(START_INDEX, END_INDEX); 
+      return str.substr(START_INDEX, END_INDEX);
     }
 
     return str;
-}
+  };
 
 
-/**
- * 
- * _isArray
- * @param {}
- * @return {boolean}
- * 
- */
+  /**
+   *
+   * _isArray
+   * @param {}
+   * @return {boolean}
+   *
+   */
 
-function _isArray(value) {
+  f2._isArray = function(value) {
     var type = $.type(value);
 
     return type === 'array';
-}
+  };
 
 
-/**
- * 
- * _isObject
- * @param {}
- * @return {boolean}
- * 
- */
+  /**
+   *
+   * _isObject
+   * @param {}
+   * @return {boolean}
+   *
+   */
 
-function _isObject(value) {
+  f2._isObject = function(value) {
     var type = $.type(value);
 
     return type === 'object';
-}
+  };
 
 
-/**
- * 
- * _isScalar
- * @param {}
- * @return {boolean}
- * 
- */
+  /**
+   *
+   * _isScalar
+   * @param {}
+   * @return {boolean}
+   *
+   */
 
-function _isScalar(value) {
+  f2._isScalar = function(value) {
     var type = $.type(value);
 
-    return (type == "number" ||
-        type == "string" ||
-        type == "boolean" ||
-        type == "null");
-}
+    return (type === "number" ||
+      type === "string" ||
+      type === "boolean" ||
+      type === "null");
+  };
+// })(this);
--- tests/flatten2.test.js
+++ tests/flatten2.test.js
@@ -1,248 +1,246 @@
 describe('flatten2', function() {
 
 
-    describe('flatten', function() {
-    
-        var data,
-        flattenedData;
+  describe('flatten', function() {
+    var data;
+    var flattenedData;
 
-        beforeEach(function() {
-            data = {
-                "patriots": {
-                    "coaches": {
-                        "head": "bill",
-                        "oline": "scarnecchia"
-                    }
-                },
-                "myNumber": 12,
-                "myBoolean": true,
-                "myString": 'hello',
-                "myNull": null,
-                "nestedTypes": {
-                    "myNumber": 12,
-                    "myBoolean": true,
-                    "myString": 'hello',
-                    "myNull": null,
-                }
-            };
+    beforeEach(function() {
+      data = {
+        "patriots": {
+          "coaches": {
+            "head": "bill",
+            "oline": "scarnecchia"
+          }
+        },
+        "myNumber": 12,
+        "myBoolean": true,
+        "myString": 'hello',
+        "myNull": null,
+        "nestedTypes": {
+          "myNumber": 12,
+          "myBoolean": true,
+          "myString": 'hello',
+          "myNull": null,
+        }
+      };
 
-            flattenedData = flatten(data);
-        });
-
-        it('should concat nested object keys', function() {
-            expect('patriots/coaches/head' in flattenedData).toBe(true);
-            expect('patriots/coaches/oline' in flattenedData).toBe(true);
-        });
-
-        it('should maintain values for concatted nested keys', function() {
-            expect(flattenedData['patriots/coaches/head']).toBe('bill');
-            expect(flattenedData['patriots/coaches/oline']).toBe('scarnecchia');
-        });
-
-        it('should maintain scalar data types', function() {
-            expect(typeof flattenedData.myNumber).toBe('number');
-            expect(typeof flattenedData.myBoolean).toBe('boolean');
-            expect(typeof flattenedData.myString).toBe('string');
-            expect(flattenedData.myNull).toBe(null);
-        });
-
-        it('should maintain nested scalar data types', function() {
-            expect(typeof flattenedData['nestedTypes/myNumber']).toBe('number');
-            expect(typeof flattenedData['nestedTypes/myBoolean']).toBe('boolean');
-            expect(typeof flattenedData['nestedTypes/myString']).toBe('string');
-            expect(flattenedData['nestedTypes/myNull']).toBe(null);
-        });
-
-        it('should flatten all object values', function() {
-            expect(isFlattened(flattenedData)).toBe(true);
-            expect(isFlattened(data)).toBe(false);
-        });
-
+      flattenedData = f2.flatten(data);
     });
 
-
-describe('flattenMany', function() {
-
-        var data;
-
-        beforeEach(function() {
-            data = [
-                {
-                    "title": "Creativity Inc",
-                    "author": "Ed Catmull",
-                    "meta": {
-                        "keywords": "business,leadership",
-                        "genre": "businees"
-                    }
-                },
-                {
-                    "title": "Ego is the Enemy",
-                    "author": "Ryan Holiday",
-                    "meta": {
-                        "keywords": "philosophy",
-                        "genre": "philosophy"
-                    }
-                }
-            ]; 
-        });
-
-        it('flatten all objects in an array', function() {
-            var flattenedData = flattenMany(data);
-
-            var isAllFlattened = true;
-
-            for (var i = 0, n = flattenedData.length; i < n; i++) {
-                var currentObject = flattenedData[i];
-                if (!isFlattened(currentObject)) {
-                    isAllFlattened = false;
-                    break;
-                }
-            }
-
-            expect(isAllFlattened).toBe(true);
-        });
-
+    it('should concat nested object keys', function() {
+      expect('patriots/coaches/head' in flattenedData).toBe(true);
+      expect('patriots/coaches/oline' in flattenedData).toBe(true);
     });
 
-
-    describe('_createKeyPath', function() {
-
-        var PATH,
-        KEY,
-        DELIMITER;
-
-        beforeEach(function() {
-            PATH = 'hello/';
-            KEY = 'world';
-            DELIMITER = '/';
-        });
-
-        it('should concat args', function() {
-            var keyPath = _createKeyPath(PATH, KEY, DELIMITER);
-
-            expect(keyPath).toBe('hello/world/'); 
-        });
-    
+    it('should maintain values for concatted nested keys', function() {
+      expect(flattenedData['patriots/coaches/head']).toBe('bill');
+      expect(flattenedData['patriots/coaches/oline']).toBe('scarnecchia');
     });
 
-
-    describe('_stripRight', function() {
-
-        var DELIMITER,
-        STR;
-
-        beforeEach(function() {
-            DELIMITER = '/';
-            STR = 'hello/world/';
-        });
-
-        it('should remove right most char if delimiter', function() {
-            var strippedPath = _stripRight(STR, DELIMITER);
-
-            expect(strippedPath).toBe('hello/world'); 
-        });
-
-        it('should return original string if right most char is not delimter', function() {
-            var notUsedChar = '!';
-            
-            var strippedPath = _stripRight(STR, notUsedChar);
-
-            expect(strippedPath).toBe('hello/world/'); 
-        });
-    
+    it('should maintain scalar data types', function() {
+      expect(typeof flattenedData.myNumber).toBe('number');
+      expect(typeof flattenedData.myBoolean).toBe('boolean');
+      expect(typeof flattenedData.myString).toBe('string');
+      expect(flattenedData.myNull).toBe(null);
     });
 
-
-    describe('_isScalar', function() {
-
-        var scalars;
-
-        beforeEach(function() {
-            scalars = {
-                "myNumber": 12,
-                "myBoolean": true,
-                "myString": 'hello',
-                "myNull": null
-            };
-        });
-
-        it('should return true when passed number', function() {
-            expect(_isScalar(scalars.myNumber)).toBe(true);
-        });
-
-        it('should return true when passed boolean', function() {
-            expect(_isScalar(scalars.myBoolean)).toBe(true);
-        });
-
-        it('should return true when passed string', function() {
-            expect(_isScalar(scalars.myString)).toBe(true);
-        });
-
-        it('should return true when passed null', function() {
-            expect(_isScalar(scalars.myNull)).toBe(true);
-        });
-    
+    it('should maintain nested scalar data types', function() {
+      expect(typeof flattenedData['nestedTypes/myNumber']).toBe('number');
+      expect(typeof flattenedData['nestedTypes/myBoolean']).toBe('boolean');
+      expect(typeof flattenedData['nestedTypes/myString']).toBe('string');
+      expect(flattenedData['nestedTypes/myNull']).toBe(null);
     });
 
-
-    describe('_isArray', function() {
-
-        var scalars;
-
-        beforeEach(function() {
-            scalars = {
-                "myNumber": 12,
-                "myBoolean": true,
-                "myString": 'hello',
-                "myNull": null
-            };
-        });
-
-        it('should return true when passed array', function() {
-            var myArray = [];
-
-            expect(_isArray(myArray)).toBe(true);
-        });
-
-        it('should return false when passed scalar or object', function() {
-            expect(_isArray(scalars.myNumber)).toBe(false);
-            expect(_isArray(scalars.myBoolean)).toBe(false);
-            expect(_isArray(scalars.myString)).toBe(false);
-            expect(_isArray(scalars.myNull)).toBe(false);
-            expect(_isArray({})).toBe(false);
-        });
-
+    it('should flatten all object values', function() {
+      expect(isFlattened(flattenedData)).toBe(true);
+      expect(isFlattened(data)).toBe(false);
     });
 
+  });
 
-    describe('_isObject', function() {
 
-        var scalars;
+  describe('flattenMany', function() {
 
-        beforeEach(function() {
-            scalars = {
-                "myNumber": 12,
-                "myBoolean": true,
-                "myString": 'hello',
-                "myNull": null
-            };
-        });
+    var data;
 
-        it('should return true when passed object', function() {
-            var myObject = {};
-
-            expect(_isObject(myObject)).toBe(true);
-        });
-
-        it('should return false when passed scalar or array', function() {
-            expect(_isObject(scalars.myNumber)).toBe(false);
-            expect(_isObject(scalars.myBoolean)).toBe(false);
-            expect(_isObject(scalars.myString)).toBe(false);
-            expect(_isObject(scalars.myNull)).toBe(false);
-            expect(_isObject([])).toBe(false);
-        });
-
+    beforeEach(function() {
+      data = [{
+          "title": "Creativity Inc",
+          "author": "Ed Catmull",
+          "meta": {
+            "keywords": "business,leadership",
+            "genre": "businees"
+          }
+        },
+        {
+          "title": "Ego is the Enemy",
+          "author": "Ryan Holiday",
+          "meta": {
+            "keywords": "philosophy",
+            "genre": "philosophy"
+          }
+        }
+      ];
     });
+
+    it('flatten all objects in an array', function() {
+      var flattenedData = f2.flattenMany(data);
+
+      var isAllFlattened = true;
+
+      for (var i = 0, n = flattenedData.length; i < n; i++) {
+        var currentObject = flattenedData[i];
+        if (!isFlattened(currentObject)) {
+          isAllFlattened = false;
+          break;
+        }
+      }
+
+      expect(isAllFlattened).toBe(true);
+    });
+
+  });
+
+
+  describe('_createKeyPath', function() {
+
+    var PATH,
+      KEY,
+      DELIMITER;
+
+    beforeEach(function() {
+      PATH = 'hello/';
+      KEY = 'world';
+      DELIMITER = '/';
+    });
+
+    it('should concat args', function() {
+      var keyPath = f2._createKeyPath(PATH, KEY, DELIMITER);
+
+      expect(keyPath).toBe('hello/world/');
+    });
+
+  });
+
+
+  describe('_stripRight', function() {
+
+    var DELIMITER,
+      STR;
+
+    beforeEach(function() {
+      DELIMITER = '/';
+      STR = 'hello/world/';
+    });
+
+    it('should remove right most char if delimiter', function() {
+      var strippedPath = f2._stripRight(STR, DELIMITER);
+
+      expect(strippedPath).toBe('hello/world');
+    });
+
+    it('should return original string if right most char is not delimter', function() {
+      var notUsedChar = '!';
+
+      var strippedPath = f2._stripRight(STR, notUsedChar);
+
+      expect(strippedPath).toBe('hello/world/');
+    });
+
+  });
+
+
+  describe('_isScalar', function() {
+
+    var scalars;
+
+    beforeEach(function() {
+      scalars = {
+        "myNumber": 12,
+        "myBoolean": true,
+        "myString": 'hello',
+        "myNull": null
+      };
+    });
+
+    it('should return true when passed number', function() {
+      expect(f2._isScalar(scalars.myNumber)).toBe(true);
+    });
+
+    it('should return true when passed boolean', function() {
+      expect(f2._isScalar(scalars.myBoolean)).toBe(true);
+    });
+
+    it('should return true when passed string', function() {
+      expect(f2._isScalar(scalars.myString)).toBe(true);
+    });
+
+    it('should return true when passed null', function() {
+      expect(f2._isScalar(scalars.myNull)).toBe(true);
+    });
+
+  });
+
+
+  describe('_isArray', function() {
+
+    var scalars;
+
+    beforeEach(function() {
+      scalars = {
+        "myNumber": 12,
+        "myBoolean": true,
+        "myString": 'hello',
+        "myNull": null
+      };
+    });
+
+    it('should return true when passed array', function() {
+      var myArray = [];
+
+      expect(f2._isArray(myArray)).toBe(true);
+    });
+
+    it('should return false when passed scalar or object', function() {
+      expect(f2._isArray(scalars.myNumber)).toBe(false);
+      expect(f2._isArray(scalars.myBoolean)).toBe(false);
+      expect(f2._isArray(scalars.myString)).toBe(false);
+      expect(f2._isArray(scalars.myNull)).toBe(false);
+      expect(f2._isArray({})).toBe(false);
+    });
+
+  });
+
+
+  describe('_isObject', function() {
+
+    var scalars;
+
+    beforeEach(function() {
+      scalars = {
+        "myNumber": 12,
+        "myBoolean": true,
+        "myString": 'hello',
+        "myNull": null
+      };
+    });
+
+    it('should return true when passed object', function() {
+      var myObject = {};
+
+      expect(f2._isObject(myObject)).toBe(true);
+    });
+
+    it('should return false when passed scalar or array', function() {
+      expect(f2._isObject(scalars.myNumber)).toBe(false);
+      expect(f2._isObject(scalars.myBoolean)).toBe(false);
+      expect(f2._isObject(scalars.myString)).toBe(false);
+      expect(f2._isObject(scalars.myNull)).toBe(false);
+      expect(f2._isObject([])).toBe(false);
+    });
+
+  });
 
 
 });
@@ -255,19 +253,19 @@ describe('flattenMany', function() {
  */
 
 function isFlattened(data) {
-    var isFlattened = true;
+  var isFlattened = true;
 
-    for (var key in data) {
-        var value = data[key];
-        var valueType = typeof value;
+  for (var key in data) {
+    var value = data[key];
+    var valueType = typeof value;
 
-        if (valueType === 'object' &&
-            value !== null &&
-            !(Array.isArray(value))) {
-            isFlattened = false;
-            break;
-        }
+    if (valueType === 'object' &&
+      value !== null &&
+      !(Array.isArray(value))) {
+      isFlattened = false;
+      break;
     }
+  }
 
-    return isFlattened;
+  return isFlattened;
 }
