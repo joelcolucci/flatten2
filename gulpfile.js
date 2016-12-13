74c8b0fd2d8cc9a9c0c51535a02fe176c0bf272b
Joel Colucci <joelcolucci@gmail.com>
2016-12-12 20:19:15 -0500

    chore: implement basic build task

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const path = require('path');
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')();
 
+const del = require('del');
 const KarmaServer = require('karma').Server;
 
 const pkg = {
@@ -23,6 +24,12 @@ const paths = {
   karma: 'karma.conf.js'
 };
 
+gulp.task('clean:dist', () => {
+  return del([
+    'dist'
+  ]);
+});
+
 gulp.task('run:lint', () => {
   return gulp.src(paths.js)
     .pipe($.excludeGitignore())
@@ -36,4 +43,15 @@ gulp.task('run:test', (done) => {
     configFile: path.join(__dirname, paths.karma),
     singleRun: true
   }, done).start();
+});
+
+gulp.task('run:build', ['clean:dist'], () => {
+  return gulp.src(paths.js)
+    .pipe($.minify({
+      ext: {
+        src: '.debug.js',
+        min: '.min.js'
+      }
+    }))
+    .pipe(gulp.dest(pkg.dist));
 });
