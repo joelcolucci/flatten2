3684d40f073d0edc34a498f767d9b5ba36d3d2ab
joelcolucci <joelcolucci@gmail.com>
2018-02-26 15:01:08 -0500

    chore: implement simplified build

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,11 @@
 'use strict';
 
-const path = require('path');
 
-const gulp = require('gulp');
-const $ = require('gulp-load-plugins')();
-
+const concat = require('gulp-concat');
 const del = require('del');
-const KarmaServer = require('karma').Server;
+const gulp = require('gulp');
+const rollup = require('rollup');
 
-const pkg = {
-  src: 'src',
-  dist: 'dist',
-  test: 'test'
-};
-
-const paths = {
-  js: [pkg.src + '/**/*.js'],
-  test: [pkg.test + '/spec/**/*.js'],
-  testRequire: [
-    pkg.test + '/mock/**/*.js',
-    pkg.test + '/spec/**/*.js'
-  ],
-  karma: 'karma.conf.js'
-};
 
 gulp.task('clean:dist', () => {
   return del([
@@ -30,43 +13,25 @@ gulp.task('clean:dist', () => {
   ]);
 });
 
-gulp.task('run:lint', () => {
-  return gulp.src(paths.js)
-    .pipe($.excludeGitignore())
-    .pipe($.eslint())
-    .pipe($.eslint.format())
-    .pipe($.eslint.failAfterError());
+
+gulp.task('build', ['clean:dist'], () => {
+  return gulp.src('./lib/index.js')
+    .pipe(concat('flatten2.js'))
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('run:test', (done) => {
-  new KarmaServer({
-    configFile: path.join(__dirname, paths.karma),
-    singleRun: true
-  }, done).start();
-});
 
-gulp.task('run:test:dist', (done) => {
-  new KarmaServer({
-    configFile: path.join(__dirname, paths.karma),
-    singleRun: true,
-    files: [
-      'node_modules/jquery/dist/jquery.js',
-      path.join(pkg.dist, '*.min.js'),
-      path.join(pkg.test, 'spec/**/*.js')
-    ],
-  }, done).start();
-});
-
-gulp.task('run:build', ['clean:dist'], () => {
-  return gulp.src(paths.js)
-    .pipe($.babel({
-      presets: ['es2015']
-    }))
-    .pipe($.minify({
-      ext: {
-        src: '.debug.js',
-        min: '.min.js'
-      }
-    }))
-    .pipe(gulp.dest(pkg.dist));
+gulp.task('bundle', ['clean:dist'], () => {
+  return rollup
+    .rollup({
+      input: './lib/index.js'
+    })
+    .then((bundle) => {
+      return bundle.write({
+        file: './dist/flatten2.js',
+        format: 'umd',
+        name: 'flatten2',
+        sourcemap: false
+      });
+    });
 });
